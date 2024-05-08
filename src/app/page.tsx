4b8b440c862657303dce9f120244390a78e05de4
DemwE <mateuszczarnecki360@gmail.com>
2024-05-08 12:30:24 +0200

    moved offer cards to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import {fetchOffers} from "@/db/utils";
 import Image from "next/image";
 import testImage from "./test.png";
+import OptimizedImage from "@/components/optimizedImage";
+import OfferCard from "@/components/offerCard";
 
 export default async function Home() {
   const offers = await fetchOffers();
@@ -11,18 +13,7 @@ export default async function Home() {
       <h1>Offers</h1>
       <div className="grid grid-cols-2 gap-4 justify-items-center">
         {offers.map((offer) => (
-          <a href={`/offer/${offer.offers.id}`} key={offer.offers.id} className="bg-amber-50 rounded-xl w-fit p-3 space-y-2">
-            <Image src={testImage} alt="Test Image" className="rounded-xl"/>
-            <h2 className="text-xl">{offer.car_marks.name} {offer.car_models.name}</h2>
-            {/*<h2>{offer.offers.title}</h2>*/}
-            <div className="grid grid-cols-4">
-              <p>Production: {offer.offers.production_year}r</p>
-              <p>Mileage: {offer.offers.mileage}km</p>
-              <p>Fuel: {offer.offers.fuel_type}</p>
-              <p className="font-bold">Price: {offer.offers.price}$</p>
-            </div>
-            <p>{offer.offers.description}</p>
-          </a>
+          <OfferCard offer={offer} cdn={process.env.STORAGE_PATH!}/>
         ))}
       </div>
     </main>
---
+++ src/components/offerCard.tsx
@@ -0,0 +1,21 @@
+'use client';
+import OptimizedImage from "@/components/optimizedImage";
+
+export default function OfferCard({offer, cdn}: { offer: any, cdn: string }) {
+  return (
+    <div onClick={() => location.href = `/offer/${offer.offers.id}`} key={offer.offers.id}
+         className="bg-amber-50 rounded-xl w-full xl:w-[60%] p-3 space-y-2">
+      <OptimizedImage src={cdn + offer.offers.main_image} alt={offer.offers.id}
+                      className="w-full h-96 rounded-xl"/>
+      <h2 className="text-xl">{offer.car_marks?.name} {offer.car_models?.name}</h2>
+      {/*<h2>{offer.offers.title}</h2>*/}
+      <div className="grid grid-cols-4">
+        <p>Production: {offer.offers.production_year}r</p>
+        <p>Mileage: {offer.offers.mileage}km</p>
+        <p>Fuel: {offer.offers.fuel_type}</p>
+        <p className="font-bold">Price: {offer.offers.price}$</p>
+      </div>
+      <p>{offer.offers.description}</p>
+    </div>
+  );
+}
